4bd492b1d913133006785242171f2b9135207fd5
Ozarion <cozarion@gmail.com>
2018-08-19 01:48:33 +0500

    Created the File Metadata microservice

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 
 var express = require('express');
 var cors = require('cors');
-
 // require and use "multer"...
-
+var multer = require('multer');
 var app = express();
 
+const upload = multer();
+
 app.use(cors());
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -16,6 +18,14 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name" : req.file.originalname,
+    "type" : req.file.mimetype,
+    "size" : `${req.file.size} bytes`
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
